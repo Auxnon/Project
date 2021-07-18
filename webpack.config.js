c102b52dc6f38050e6e3b92c5759641660ef0cee
Makeavoy <auxnon@gmail.com>
2021-07-18 14:56:02 -0400

    first: initial project setup

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {
+    CleanWebpackPlugin
+} = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.ts',
+    mode: 'development',//'development', production
+    plugins: [
+        new CleanWebpackPlugin({
+            dry: false,
+            cleanOnceBeforeBuildPatterns: ['**/*', '!styles', '!styles/*', '!assets', '!assets/**/*'],
+            verbose: true
+        }),
+        new HtmlWebpackPlugin({
+            inject: 'body',
+            template: './src/index.html',
+            filename: 'index.html'
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: ['style-loader','css-loader', 'sass-loader'],
+                exclude: /node_modules/,
+            },
+        ],
+    },/*
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },*/
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+};
